2a57e8488617348fd7f3576a1b51fa5573503a7e
Vasyl Romanets <romanets.vasyl@gmail.com>
2026-05-30 23:07:42 +0300

    Prefer case-insensitive globbing

--- .config/zsh/options.sh
+++ .config/zsh/options.sh
@@ -5,3 +5,4 @@ setopt CORRECT_ALL       # e.g. gti → git
 setopt HIST_IGNORE_DUPS  # don't record duplicates
 setopt HIST_IGNORE_SPACE # don't record commands starting with a space
 setopt SHARE_HISTORY     # share history across terminal sessions
+setopt NO_CASE_GLOB      # e.g. ls *.MD matches ls *.md
