6c1f78a3564871fa44baec975e7deda7ab457a0b
Vasyl Romanets <romanets.vasyl@gmail.com>
2026-05-30 23:10:29 +0300

    Skip all duplicates not only consecutive ones

--- .config/zsh/options.sh
+++ .config/zsh/options.sh
@@ -1,8 +1,8 @@
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
-setopt CORRECT_ALL       # e.g. gti → git
-setopt HIST_IGNORE_DUPS  # don't record duplicates
-setopt HIST_IGNORE_SPACE # don't record commands starting with a space
-setopt SHARE_HISTORY     # share history across terminal sessions
-setopt NO_CASE_GLOB      # e.g. ls *.MD matches ls *.md
+setopt CORRECT_ALL          # e.g. gti → git
+setopt HIST_IGNORE_ALL_DUPS # don't record duplicates
+setopt HIST_IGNORE_SPACE    # don't record commands starting with a space
+setopt SHARE_HISTORY        # share history across terminal sessions
+setopt NO_CASE_GLOB         # e.g. ls *.MD matches ls *.md
