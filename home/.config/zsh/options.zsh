c472007bcb453e6c37e12fc9fc5f42f1946b3bfc
Vasyl Romanets <romanets.vasyl@gmail.com>
2026-05-31 23:49:34 +0300

    Handle paths with spaces

--- home/.config/zsh/options.zsh
+++ home/.config/zsh/options.zsh
@@ -2,10 +2,10 @@ export CORRECT_IGNORE=".*"
 export CORRECT_IGNORE_FILE="_*"
 export SPROMPT="Correct '%F{red}%R%f' to '%F{green}%r%f' [nyae]?"
 
-ZSH_STATE_HOME=$XDG_STATE_HOME/zsh
+ZSH_STATE_HOME="$XDG_STATE_HOME/zsh"
 mkdir -p "$ZSH_STATE_HOME" # ensure directory exists before writing history
 
-HISTFILE=$ZSH_STATE_HOME/history
+HISTFILE="$ZSH_STATE_HOME/history"
 HISTSIZE=10000
 SAVEHIST=$HISTSIZE
 
