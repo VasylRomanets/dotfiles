c07b66b7d90fe4dfc7909bcb6f19068d670aa442
Vasyl Romanets <romanets.vasyl@gmail.com>
2026-06-14 13:59:32 +0300

    Add more aliases

--- home/.config/zsh/aliases.zsh
+++ home/.config/zsh/aliases.zsh
@@ -1,13 +1,16 @@
 # git
 alias g="git"
+alias gf="git fetch"
 alias gs="git status"
 alias ga="git add"
 alias gc="git commit"
+alias gpu="git pull"
 alias gp="git push"
 alias gl="git log --oneline --graph --decorate"
 alias gd="git diff"
 alias gco="git checkout"
 alias gb="git branch"
+alias gst="git stash"
 
 # grep & ripgrep
 alias grep="grep --color=auto"
@@ -18,6 +21,9 @@ alias ls="eza --color=always --group-directories-first --icons"
 alias ll="eza --color=always --group-directories-first --icons --long --git"
 alias la="eza --color=always --group-directories-first --icons --long --git --all"
 alias tree="eza --color=always --group-directories-first --icons --tree --level=2"
+
+# rm
+alias rms="trash"
 
 # Fun
 alias moo="fortune | cowsay --random --rainbow --aurora"
