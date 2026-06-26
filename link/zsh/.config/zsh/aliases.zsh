a6aae8f7149cab4d489e256b31019c43ef8d20e6
Vasyl Romanets <romanets.vasyl@gmail.com>
2026-06-26 15:04:32 +0300

    Add alias for fuzzy man page search

--- link/zsh/.config/zsh/aliases.zsh
+++ link/zsh/.config/zsh/aliases.zsh
@@ -11,6 +11,9 @@ alias ll="eza --color=always --group-directories-first --icons --long --git"
 alias la="eza --color=always --group-directories-first --icons --long --git --all"
 alias tree="eza --color=always --group-directories-first --icons --tree --level=2"
 
+# fuzzy man page search
+alias fman="print -l ${(k)commands} | fzf | xargs man"
+
 # trash
 alias rms="trash"
 
